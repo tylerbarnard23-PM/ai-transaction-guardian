934fdfe641bcbdd688d7f76c65993bc00615592f
Tyler  Barnard <tyler.barnard23@gmail.com>
2026-05-01 11:03:52 -0400

    Initialize Google Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { initGoogleAnalytics } from './lib/analytics'
+
+initGoogleAnalytics()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
